b852fa3d4087c9c10513d1fbad357547e97aa156
Muhammad Zaeem <muhammadzaeemilyas@gmail.com>
2024-02-12 18:30:14 +0500

    checking

--- lib/src/pages/print_page.dart
+++ lib/src/pages/print_page.dart
@@ -1,3 +1,5 @@
+import 'package:bluetooth_thermal_printer/bluetooth_thermal_printer.dart';
+import 'package:esc_pos_utils/esc_pos_utils.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:blue_thermal_printer/blue_thermal_printer.dart';
@@ -177,7 +179,9 @@ class _blprintState extends State<blprint> {
                     style: ElevatedButton.styleFrom(primary: Colors.brown),
                     onPressed: () async {
                       await testPrint.sample();
+
                       print('krrrrr rhaaa');
+                      //await printTicket();
                     },
                     child: Text('PRINT TEST',
                         style: TextStyle(color: Colors.white)),
@@ -189,6 +193,171 @@ class _blprintState extends State<blprint> {
         ),
       ),
     );
+  }
+
+  Future<List<int>> getTicket() async {
+    List<int> bytes = [];
+    CapabilityProfile profile = await CapabilityProfile.load();
+    final generator = Generator(PaperSize.mm80, profile);
+
+    bytes += generator.text("Demo Shop",
+        styles: PosStyles(
+          align: PosAlign.center,
+          height: PosTextSize.size2,
+          width: PosTextSize.size2,
+        ),
+        linesAfter: 1);
+
+    bytes += generator.text(
+        "18th Main Road, 2nd Phase, J. P. Nagar, Bengaluru, Karnataka 560078",
+        styles: PosStyles(align: PosAlign.center));
+    bytes += generator.text('Tel: +919591708470',
+        styles: PosStyles(align: PosAlign.center));
+
+    bytes += generator.hr();
+    bytes += generator.row([
+      PosColumn(
+          text: 'No',
+          width: 1,
+          styles: PosStyles(align: PosAlign.left, bold: true)),
+      PosColumn(
+          text: 'Item',
+          width: 5,
+          styles: PosStyles(align: PosAlign.left, bold: true)),
+      PosColumn(
+          text: 'Price',
+          width: 2,
+          styles: PosStyles(align: PosAlign.center, bold: true)),
+      PosColumn(
+          text: 'Qty',
+          width: 2,
+          styles: PosStyles(align: PosAlign.center, bold: true)),
+      PosColumn(
+          text: 'Total',
+          width: 2,
+          styles: PosStyles(align: PosAlign.right, bold: true)),
+    ]);
+
+    bytes += generator.row([
+      PosColumn(text: "1", width: 1),
+      PosColumn(
+          text: "Tea",
+          width: 5,
+          styles: PosStyles(
+            align: PosAlign.left,
+          )),
+      PosColumn(
+          text: "10",
+          width: 2,
+          styles: PosStyles(
+            align: PosAlign.center,
+          )),
+      PosColumn(text: "1", width: 2, styles: PosStyles(align: PosAlign.center)),
+      PosColumn(text: "10", width: 2, styles: PosStyles(align: PosAlign.right)),
+    ]);
+
+    bytes += generator.row([
+      PosColumn(text: "2", width: 1),
+      PosColumn(
+          text: "Sada Dosa",
+          width: 5,
+          styles: PosStyles(
+            align: PosAlign.left,
+          )),
+      PosColumn(
+          text: "30",
+          width: 2,
+          styles: PosStyles(
+            align: PosAlign.center,
+          )),
+      PosColumn(text: "1", width: 2, styles: PosStyles(align: PosAlign.center)),
+      PosColumn(text: "30", width: 2, styles: PosStyles(align: PosAlign.right)),
+    ]);
+
+    bytes += generator.row([
+      PosColumn(text: "3", width: 1),
+      PosColumn(
+          text: "Masala Dosa",
+          width: 5,
+          styles: PosStyles(
+            align: PosAlign.left,
+          )),
+      PosColumn(
+          text: "50",
+          width: 2,
+          styles: PosStyles(
+            align: PosAlign.center,
+          )),
+      PosColumn(text: "1", width: 2, styles: PosStyles(align: PosAlign.center)),
+      PosColumn(text: "50", width: 2, styles: PosStyles(align: PosAlign.right)),
+    ]);
+
+    bytes += generator.row([
+      PosColumn(text: "4", width: 1),
+      PosColumn(
+          text: "Rova Dosa",
+          width: 5,
+          styles: PosStyles(
+            align: PosAlign.left,
+          )),
+      PosColumn(
+          text: "70",
+          width: 2,
+          styles: PosStyles(
+            align: PosAlign.center,
+          )),
+      PosColumn(text: "1", width: 2, styles: PosStyles(align: PosAlign.center)),
+      PosColumn(text: "70", width: 2, styles: PosStyles(align: PosAlign.right)),
+    ]);
+
+    bytes += generator.hr();
+
+    bytes += generator.row([
+      PosColumn(
+          text: 'TOTAL',
+          width: 6,
+          styles: PosStyles(
+            align: PosAlign.left,
+            height: PosTextSize.size4,
+            width: PosTextSize.size4,
+          )),
+      PosColumn(
+          text: "160",
+          width: 6,
+          styles: PosStyles(
+            align: PosAlign.right,
+            height: PosTextSize.size4,
+            width: PosTextSize.size4,
+          )),
+    ]);
+
+    bytes += generator.hr(ch: '=', linesAfter: 1);
+
+    // ticket.feed(2);
+    bytes += generator.text('Thank you!',
+        styles: PosStyles(align: PosAlign.center, bold: true));
+
+    bytes += generator.text("26-11-2020 15:22:45",
+        styles: PosStyles(align: PosAlign.center), linesAfter: 1);
+
+    bytes += generator.text(
+        'Note: Goods once sold will not be taken back or exchanged.',
+        styles: PosStyles(align: PosAlign.center, bold: false));
+    bytes += generator.cut();
+    return bytes;
+  }
+
+  Future<void> printTicket() async {
+    String isConnected = 'true';
+    if (isConnected == "true") {
+      List<int> bytes = await getTicket();
+      final result = await BluetoothThermalPrinter.writeBytes(bytes);
+      print('reeeee');
+      print("Print $result");
+    } else {
+      print('not connected');
+      //Hadnle Not Connected Senario
+    }
   }
 
   List<DropdownMenuItem<BluetoothDevice>> _getDeviceItems() {
@@ -210,9 +379,12 @@ class _blprintState extends State<blprint> {
 
   void _connect() {
     if (_device != null) {
+      print('device hein');
+      bluetooth.connect(_device!);
       bluetooth.isConnected.then((isConnected) {
         if (isConnected == true) {
           bluetooth.connect(_device!).catchError((error) {
+            print(error);
             setState(() => _connected = false);
           });
           setState(() => _connected = true);
